582d41d17109248ba46753a54d2930dd7c7be568
Paulo Diniz <paulo.rc.diniz@gmail.com>
2016-04-21 00:25:00 +0200

    fetching data about speeches

--- lib/speech.ex
+++ lib/speech.ex
@@ -1,7 +1,9 @@
 defmodule Speech do
-
+  require IEx
+  
   @sessions [["082.2.55.O", "15/04/16"]]
   @list_speeches_url "http://www.camara.gov.br/sitcamaraws/SessoesReunioes.asmx/ListarDiscursosPlenario"
+  @user_agent  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.112 Safari/537.36" 
 
   # Fetch data for all the sessions
   def fetch_sessions_speeches do
@@ -11,7 +13,29 @@ defmodule Speech do
   # Fetch metadata off all speeches in a sessions ocurring in a specific date
   def fetch_session_speeches([session_id, date]) do
     url_for_session(session_id, date)
-    |> HTTPotion.get(headers: ["User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.112 Safari/537.36"])
+    |> HTTPotion.get(headers: ["User-Agent": @user_agent ])
+    |> parse_response
+  end
+
+  def parse_response(%HTTPotion.Response{body: body, headers: headers, status_code: 200}) do
+    [{"discursos", [], speeches}] = Floki.find(body, "discursos")
+    Enum.map(speeches, &extract_data_from_speech(&1))
+  end
+
+  def extract_data_from_speech(raw_speech) do
+    {"discurso", [],[
+        { "orador", _, _},
+        { "horainiciodiscurso", _, _ },
+        { "txtindexacao", _, _ },
+        { "numeroquarto", _, [numquarto] },
+        { "numeroinsercao", _, [numinsercao]},
+        { "sumario", _, _}
+      ]} = raw_speech
+
+    { numinsercao, _ } = Integer.parse(numinsercao)
+    { numquarto, _ } = Integer.parse(numquarto)
+
+    %{numeroinsercao: numinsercao, numeroquarto: numquarto}
   end
 
   def url_for_session(session_id, date) do
--- test/speech_test.exs
+++ test/speech_test.exs
@@ -5,4 +5,21 @@ defmodule SpeechTest do
   test "url for session" do
     assert Speech.url_for_session("25.82", "16/02/2013") == "http://www.camara.gov.br/sitcamaraws/SessoesReunioes.asmx/ListarDiscursosPlenario?codigoSessao=25.82&dataIni=16/02/2013&dataFim=16/02/2013&parteNomeParlamentar=&siglaPartido=&siglaUF="
   end
+
+  test "extract info from speech" do
+    raw_speech = {"discurso", [],
+      [{"orador", [],
+          [{"numero", [], ["1"]}, {"nome", [], ["EDUARDO CUNHA (PRESIDENTE)"]},
+            {"partido", [], ["PMDB        "]}, {"uf", [], ["RJ"]}]},
+        {"horainiciodiscurso", [], ["15/4/2016 09:00:00"]},
+        {"txtindexacao", [],
+          ["USO DA PALAVRA, DCR 1/2015, DENÚNCIA POR CRIME DE RESPONSABILIDADE, DILMA ROUSSEFF, PRESIDENTE DA REPÚBLICA, IMPEACHMENT, COMISSÃO ESPECIAL, PARECER, ADMISSIBILIDADE, ESCLARECIMENTO."]},
+        {"numeroquarto", [], ["4"]}, {"numeroinsercao", [], ["0"]},
+        {"sumario", [],
+          ["Esclarecimentos ao Plenário sobre inscrição de Deputados para uso da palavra."]}]}
+
+    speech = Speech.extract_data_from_speech(raw_speech)
+    assert speech.numeroquarto == 4
+    assert speech.numeroinsercao == 0
+  end
 end
